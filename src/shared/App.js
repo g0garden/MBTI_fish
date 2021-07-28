2b05b4323c9d1e0ce2460e1810aa75af137e9ca8
kyngmn <79073979+kyngmn@users.noreply.github.com>
2021-07-28 23:52:03 +0900

    try react-snap

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,20 +1,19 @@
 import "./App.css";
 import React from "react";
 import { ConnectedRouter } from "connected-react-router";
-import { Route, Switch} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { history } from "../redux-toolkit/configureStore";
 import { Main, Quiz, Result, NotFound } from "../pages";
 
 function App() {
-  
   return (
     <>
       <ConnectedRouter history={history}>
         <Switch>
-          <Route exact path="/" component={Main}/>
+          <Route exact path="/" component={Main} />
           <Route exact path="/quiz" component={Quiz} />
-          <Route exact path="/result/:fishname" component={Result}/>
-          <Route exact component={NotFound}/>
+          <Route exact path="/result/:fishname" component={Result} />
+          <Route exact component={NotFound} />
         </Switch>
       </ConnectedRouter>
     </>
